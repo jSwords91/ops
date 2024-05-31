848eb3e603ac28b363ea2f4d764e45e43718215d
jSwords91 <78209426+jSwords91@users.noreply.github.com>
2024-05-31 09:36:23 +0100

    fixed path

--- MLOps/tests/tests_dataLoader.py
+++ MLOps/tests/tests_dataLoader.py
@@ -5,7 +5,7 @@ from src.dataLoader import DataLoader
 class TestDataLoader(unittest.TestCase):
 
     def test_load_data(self):
-        data_loader = DataLoader(file_path="data/diabetes.csv")
+        data_loader = DataLoader(file_path="data/raw/diabetes.csv")
         data = data_loader.load_data()
         self.assertIsInstance(data, pd.DataFrame)
         self.assertFalse(data.empty)
